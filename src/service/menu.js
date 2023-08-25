c051f8c1bc54c6e0b49321d73768dc3640cce6bd
Oleksii Verbytskyi <alverba88@gmail.com>
2023-08-25 10:47:02 +0300

    modify functionality of menu

--- src/service/menu.js
+++ src/service/menu.js
@@ -18,68 +18,21 @@ document.querySelectorAll('.menu__link').forEach(link => {
   });
 });
 
-//
-//
-// let isMenuVisible = true;
-// let isCursorOverHeader = false;
-// const header = document.querySelector('.header');
-// const sections = document.querySelectorAll('.section');
-//
-// document.querySelectorAll('.menu__link').forEach(link => {
-//   link.addEventListener('click', function(event) {
-//     event.preventDefault();
-//     const targetSection = link.getAttribute('href');
-//
-//     document.querySelectorAll('.menu__item').forEach(item => {
-//       item.classList.remove('active');
-//     });
-//
-//     link.parentElement.classList.add('active');
-//
-//     const targetElement = document.querySelector(targetSection);
-//     const yOffset = -104;
-//     const yPosition = targetElement.getBoundingClientRect().top + window.pageYOffset + yOffset;
-//
-//     window.scrollTo({
-//       top: yPosition,
-//       behavior: 'smooth'
-//     });
-//   });
-// });
-//
-// header.addEventListener('mouseenter', () => {
-//   isCursorOverHeader = true;
-//   toggleMenuVisibility();
-// });
-//
-// header.addEventListener('mouseleave', () => {
-//   isCursorOverHeader = false;
-//   toggleMenuVisibility();
-// });
-//
-// window.addEventListener('scroll', () => {
-//   toggleMenuVisibility();
-// });
-//
-// function toggleMenuVisibility() {
-//   const currentScrollPos = window.pageYOffset;
-//
-//   if (currentScrollPos === 0) {
-//     isMenuVisible = true;
-//   } else if (!isCursorOverHeader && !isScrolledToSection(currentScrollPos)) {
-//     isMenuVisible = false;
-//   }
-//
-//   if (isMenuVisible) {
-//     header.classList.remove('hidden');
-//   } else {
-//     header.classList.add('hidden');
-//   }
-// }
-//
-// function isScrolledToSection(scrollPos) {
-//   return Array.from(sections).some(section => {
-//     const sectionTop = section.getBoundingClientRect().top + window.pageYOffset;
-//     return sectionTop <= scrollPos;
-//   });
-// }
+const menuItems = document.querySelectorAll('.menu__item'),
+  sections=document.querySelectorAll('section[id]');
+
+window.addEventListener('scroll', () => {
+  let currentSection = '';
+  for (const section of sections) {
+    if (section.offsetTop <= window.scrollY + 140) {
+      currentSection = section.getAttribute('id');
+    }
+  }
+  menuItems.forEach(link => {
+    link.classList.remove('active');
+    if (link.querySelector('a').getAttribute('href') === `#${currentSection}`) {
+      link.classList.add('active');
+    }
+  });
+});
+
